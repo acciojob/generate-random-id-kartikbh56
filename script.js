3e2d2ff5b11c4fd6ed5c6e73a1748e1ca432e032
Kartik B H <95575463+kartikbh56@users.noreply.github.com>
2025-06-29 16:58:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function makeid(l) {
   // write your code here
+	let res = ""
+	let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+	for(let i=0;i<l;i++){
+		res += chars.charAt(Math.floor(Math.random()*chars.length))
+	}
+	return res
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(l)); 
